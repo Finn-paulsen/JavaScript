dfb5a0449d609ef0e7972bec9127443b5f649237
Finn Paulsen <Finnpaulsen10@web.de>
2023-10-09 15:54:06 +0200

    Add files via upload

---
+++ To-Do-Liste Erklärung.js
@@ -0,0 +1,15 @@
+let neuesItemFueDieEinkaufsListe={title:"",count:0,done:false, store:""}
+let einkaufsliste = []
+
+function addToList(){
+    console.log("Add To list Exec");
+}
+
+function changeListItem(welcherItem, WertDerGeändertWerdenSoll, dieVerändern){
+// oder mehrere Funktionen für NameÄndern, Stückzahländer etc.    
+}
+
+
+function deleteItemFromList(welcheItem){
+
+}
